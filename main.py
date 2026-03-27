6c8cb55ee56c7f1250e229bffa1ad23248be1fe6
vincenttsai2015 <vincentthunder2011@gmail.com>
2026-03-27 12:30:45 +0800

    Update bmnist inference in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import time
-
+import numpy as np
 from tqdm import tqdm
 import torch
 from torch.nn.utils import clip_grad_norm_
@@ -13,6 +13,26 @@ from models import get_flow_model
 from datasets import get_dataset
 from utils import seed_all, load_config, get_optimizer, get_scheduler, count_parameters, recursive_to_device
 from visualize import get_vis
+from evaluation.fid import get_fid, calculate_activation_statistics, InceptionV3
+
+def cal_elbo(model, dataset, max_sample=1000, batch_size=100, method='ode', n_step=500, tmax=0.995, device='cuda'):
+    generator = torch.Generator()
+    generator.manual_seed(42)
+    idx = torch.randperm(len(dataset), generator=generator)[:max_sample]
+    subset = [dataset[i][0] for i in idx]
+
+    nlls = []
+    model.eval()
+    with torch.no_grad():
+        for i in range(0, max_sample, batch_size):
+            x = torch.stack(subset[i:i + batch_size], dim=0).to(device)
+            nll = model.compute_elbo(method, x.to(device), n_step, tmax=tmax, verbose=True)
+            nlls.append(nll.item())
+            print(f'NLL: {sum(nlls) / len(nlls):.4f}')
+    nlls = np.array(nlls)
+    print(f'Avg NLL: {nlls.mean():.4f} ± {nlls.std():.4f}')
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -109,7 +129,7 @@ if __name__ == '__main__':
                 # Validation
                 if global_step % config.train.val_freq == 0:
                     avg_val_loss = validate(valid_loader)
-                    sample('euler', 'valid', config.get('sample_max_batch', None))
+                    sample('euler', 'valid', config.train.batch_size)
                     if config.train.scheduler.type == 'plateau':
                         scheduler.step(avg_val_loss)
                     else:
@@ -169,7 +189,7 @@ if __name__ == '__main__':
         return val_loss
 
 
-    def sample(method='euler', split='valid', max_batch=None):
+    def sample(method='euler', split='valid', max_batch=config.train.batch_size):
         with torch.no_grad():
             model.eval()
             if not config.conditioned:
@@ -179,16 +199,50 @@ if __name__ == '__main__':
                 traj = visualizer(model, dataloader, method, global_step, max_batch=max_batch)
         return traj
 
-
     try:
         if args.mode == 'train':
             train()
             print('Training finished!')
-        if args.mode == 'inf' and args.resume is None:
-            print('[WARNING]: inference mode without loading a pretrained model')
+            sample('ode', 'valid', None)
+            print('Sampling finished!')
+        if args.mode == 'inf': 
+            if args.resume is None: print('[WARNING]: inference mode without loading a pretrained model')
+            else:
+                print('Loading model for inference...')
+                ckpt = torch.load(args.resume, map_location=args.device)
+                model.load_state_dict(ckpt['model'])
+                model.eval()
+                print('Model loading complete!')
+                samples = []
+                total_sample = len(test_set)
+                n_batch = total_sample // config.train.batch_size
+                with torch.no_grad():
+                    for _ in tqdm(range(n_batch)):
+                        s = model.sample('ode', n_sample=config.train.batch_size, n_steps=300, device=args.device)
+                        samples.append(s.detach().cpu())
+                    samples = torch.cat(samples)
+                    img = (samples[..., 0] > samples[..., 1]).float().view(-1, 1, 28, 28).expand(-1, 3, -1, -1)
+                    print(f'Generated samples shape: {img.shape}')
 
-        sample('ode', 'valid', None)
-        print('Sampling finished!')
+                # sample('ode', 'test', None)
+                print('Sampling finished!')
+                
+                print('Calculating FID...')
+                gt = []
+                for samples, *_ in tqdm(test_loader):
+                    img = (samples[..., 0] > samples[..., 1]).float().view(-1, 1, 28, 28).expand(-1, 3, -1, -1)
+                    gt.append(img)
+                gt = torch.cat(gt, dim=0)
+                block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
+                inception = InceptionV3([block_idx]).to(args.device)
+                inception.eval()
+                mu, sigma = calculate_activation_statistics(gt, inception, args.device, batch_size=256)
+                np.savez('bmnist_fid.npz', mu=mu, sigma=sigma)
+                print('Done! Statistics saved to bmnist_fid.npz')
+
+                fid = get_fid(img, args.device, batch_size=config.train.batch_size)
+                print(f'FID: {fid:.4f}')
+                cal_elbo(model, test_set, max_sample=total_sample, batch_size=config.train.batch_size, method='ode', n_step=500, tmax=0.995, device=args.device)
         time.sleep(3)  # Wait for the last tensorboard logs to be written
     except KeyboardInterrupt:
         print('Terminating...')
